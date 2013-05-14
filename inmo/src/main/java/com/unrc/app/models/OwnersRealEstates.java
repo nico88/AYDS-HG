11e772efff9af9b778526d8abe5b5d187266cf59
nico88 <nico.neg88@gmail.com>
2013-05-14 17:08:57 -0300

    Commit message

--- inmo/src/main/java/com/unrc/app/models/OwnersRealEstates.java
+++ inmo/src/main/java/com/unrc/app/models/OwnersRealEstates.java
@@ -4,6 +4,7 @@ import org.javalite.activejdbc.Model;
 
 public class OwnersRealEstates extends Model {
   static{
-	   
+	  validatePresenceOf("owner_id");
+      validatePresenceOf("real_estate_id");
   }
 }
