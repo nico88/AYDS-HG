41a29e83871fe66055220c3ab1d37ce2f01b791c
Ignacio Herrero <herrero.olivari@gmail.com>
2013-06-01 19:23:34 -0300

    Se agrego el scaffolding para el web service de búsqueda |  Se genero una WebApp para testear el web service

--- inmo/src/main/java/com/unrc/app/Inmo.java
+++ inmo/src/main/java/com/unrc/app/Inmo.java
@@ -21,12 +21,64 @@ import spark.*;
 public class Inmo {
     public static void main( String[] args )
     {
-/*      get(new Route("/hello") {
+      get(new Route("/owners") {
          @Override
          public Object handle(Request request, Response response) {
-            return "Hello World!";
+            String city = request.queryParams("city");
+            return "jsonCallback("+"{\"owners\":\""+city+"\"}"+")";
          }
       });
-*/
+
+
+      get(new Route("/real-estates") {
+         @Override
+         public Object handle(Request request, Response response) {
+            String city = request.queryParams("city");
+            return "real-estate " + city;
+         }
+      });
+
+      get(new Route("/buildings") {
+         @Override
+         public Object handle(Request request, Response response) {
+            //check type is land|farm|house|apartment|office|garage
+            //check city is [:alpha:]+
+            //check pmax and pmin is [:digit:]+
+            String type = request.queryParams("type");
+            String city = request.queryParams("city");
+            String pmin = request.queryParams("pmin");
+            String pmax = request.queryParams("pmax");
+
+            return "buildings";
+         }
+      });
+
+      get(new Route("/land") {
+         @Override
+         public Object handle(Request request, Response response) {
+            //check city is [:alpha:]+
+            //check pmax and pmin is [:digit:]+
+            String city = request.queryParams("city");
+            String pmin = request.queryParams("pmin");
+            String pmax = request.queryParams("pmax");
+
+            return "buildings-land";
+         }
+      });
+
+
+      get(new Route("/famr") {
+         @Override
+         public Object handle(Request request, Response response) {
+            //check city is [:alpha:]+
+            //check pmax and pmin is [:digit:]+
+            String city = request.queryParams("city");
+            String pmin = request.queryParams("pmin");
+            String pmax = request.queryParams("pmax");
+
+            return "buildings-farm";
+         }
+      });
+
     }
 }
